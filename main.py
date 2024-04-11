e17a80d9a3015579b6daec22504d2f381a97e909
ilyas <ilya.surgutskij11@mail.ru>
2024-04-11 14:36:21 +0500

    socrashenia

--- main.py
+++ main.py
@@ -17,23 +17,31 @@ def greet(message):
 
 
 @bot.callback_query_handler(func=lambda callback_data: True)
-def choice(callback):
+def education_lvl(callback):
     markup = types.InlineKeyboardMarkup()
     if callback.data == "Бакалавриат":
         markup.add(types.InlineKeyboardButton("Математика и Информатика", callback_data="Математика и Информатика"))
         markup.add(types.InlineKeyboardButton("Математика и Физика", callback_data="Математика и Физика"))
         bot.edit_message_text("""Прекрасный выбор! Подача документов будет возможна до 7 июля. 
-Выберите направление: """, callback.message.chat.id, callback.message.message_id,reply_markup=markup)
+Выберите направление: """, callback.message.chat.id, callback.message.message_id, reply_markup=markup)
     if callback.data == "Магистратура":
-        markup.add(types.InlineKeyboardButton("Цифровизация образования: проектирование, сопровождение, экспертиза", callback_data="Математика и Информатика"))
+        markup.add(types.InlineKeyboardButton("Цифровизация образования: проектирование, сопровождение, экспертиза",
+                                              callback_data="Цифровизация"))
         bot.edit_message_text("""Прекрасный выбор! Подача документов будет возможна до 22 августа. 
-Выберите направление: """, callback.message.chat.id, callback.message.message_id,reply_markup=markup)
+Выберите направление: """, callback.message.chat.id, callback.message.message_id, reply_markup=markup)
     if callback.data == "Аспирантура":
-        markup.add(types.InlineKeyboardButton("Математика и Информатика", callback_data="Математика и Информатика"))
-        markup.add(types.InlineKeyboardButton("Математика и Физика", callback_data="Математика и Физика"))
+        markup.add(types.InlineKeyboardButton("Методология и технология профессионального образования",
+                                              callback_data="Проф. обр"))
+        markup.add(types.InlineKeyboardButton("Общая педагогика, история педагогики и образования",
+                                              callback_data="Педагогика"))
         bot.edit_message_text("""Прекрасный выбор! Подача документов будет возможна до 20 августа. 
-Выберите направление: """, callback.message.chat.id, callback.message.message_id,reply_markup=markup)
+Выберите направление: """, callback.message.chat.id, callback.message.message_id, reply_markup=markup)
 
+
+@bot.callback_query_handler(func=lambda callback_data: True)
+def programm(callback):
+    if callback.data == "Педагогика":
+        bot.edit_message_text("Hi", callback.message.chat.id, callback.message.message_id)
 
 
 bot.infinity_polling()
