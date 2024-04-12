89b62ed6551fbe1890d849bcf6e13a7fc32420ff
ilyas <ilya.surgutskij11@mail.ru>
2024-04-12 15:03:56 +0500

    fix git please

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ Postgraduate = list()
 with open('base.json', encoding="utf-8") as f:
     templates = json.load(f)
 
-
-
 # a = templates[0]["question"][0]["На какой уровень образования вы планируете поступать?"]["Бакалавриат"]
 
 Bachelor.append(templates[0]["question"][0]["На какой уровень образования вы планируете поступать?"]["Бакалавриат"])
@@ -49,35 +47,34 @@ def education_lvl(callback):
     back = types.InlineKeyboardButton("Назад", callback_data="back")
     back_to_lvl = types.InlineKeyboardButton("Назад", callback_data="back_to_lvl")
     if callback.data == "Бакалавриат":
-<<<<<<< HEAD
-        markup.add(types.InlineKeyboardButton(Bachelor[1], callback_data="Математика и Информатика"))
-        markup.add(types.InlineKeyboardButton(Bachelor[2], callback_data="Математика и Физика"))
-        bot.edit_message_text(f"""Прекрасный выбор! {Bachelor[0]}. 
-=======
         MI = types.InlineKeyboardButton(Bachelor[1], callback_data="Математика и Информатика")
         MP = types.InlineKeyboardButton(Bachelor[2], callback_data="Математика и Физика")
         markup.add(MI, MP)
         markup.add(back)
-        bot.edit_message_text(f"""Вы выбрали уровень: бакалавриат. {Bachelor[0]}. 
->>>>>>> b88cdea85fdd6eb0040a4ca3fe4159a4a7ce4657
+        bot.edit_message_text(f"""Вы выбрали уровень: бакалавриат. {Bachelor[0]}
 Выберите направление: """, callback.from_user.id, callback.message.message_id, reply_markup=markup)
     if callback.data == "Магистратура":
-        last_ide = "Магистратура"
-        back_to_lvl = types.InlineKeyboardButton("Назад", callback_data="Магистратура")
-        markup.add(types.InlineKeyboardButton(Master[1],
-                                              callback_data="Цифровизация"))
-        bot.edit_message_text(f"""Прекрасный выбор! {Master[0]} 
+        DIG = types.InlineKeyboardButton(Master[1],
+                                         callback_data="Цифровизация")
+        markup.add(DIG)
+        bot.edit_message_text(f"""Вы выбрали уровень: магистратура {Master[0]} 
 Выберите направление: """, callback.from_user.id, callback.message.message_id, reply_markup=markup)
     if callback.data == "Аспирантура":
-        last_ide = "Аспирантура"
-        back_to_lvl = types.InlineKeyboardButton("Назад", callback_data="Аспирантура")
         markup.add(types.InlineKeyboardButton(Postgraduate[1],
                                               callback_data="Проф. обр"))
         markup.add(types.InlineKeyboardButton(Postgraduate[2],
                                               callback_data="Педагогика"))
-        bot.edit_message_text(f"""Прекрасный выбор! {Postgraduate[0]} 
+        bot.edit_message_text(f"""Вы выбрали уровень: аспирантура {Postgraduate[0]} 
 Выберите направление: """, callback.from_user.id, callback.message.message_id, reply_markup=markup)
-        bot.register_next_step_handler(callback, program)
+        bot.register_next_step_handler(callback)
+
+    if callback.data == "Математика и Информатика":
+        bot.edit_message_text("""Вступительные испытания:
+– Русский язык (ЕГЭ, не менее 40 баллов)
+– Математика (ЕГЭ, не менее 39 баллов)
+– Информатика и ИКТ
+Более подробная информация:
+https://www.surgpu.ru/Abitur/bachelor/""", callback.from_user.id, callback.message.message_id, reply_markup=markup)
 
     if callback.data == "Математика и Физика":
         markup.add(back_to_lvl)
@@ -107,10 +104,17 @@ https://www.surgpu.ru/Abitur/magistratura/""", callback.from_user.id, callback.m
 https://www.surgpu.ru/Abitur/aspirantura/""", callback.from_user.id, callback.message.message_id, reply_markup=markup)
 
     if callback.data == "Педагогика":
-        bot.edit_message_text("Hi", callback.from_user.id, callback.message.message_id, reply_markup=markup)
+        bot.edit_message_text("""– Специальная дисциплина, соответствующая
+направленности (профилю) программы
+подготовки научно-педагогических кадров в аспирантуре
+– Философия
+– Иностранный язык
+Более подробная информация
+https://www.surgpu.ru/Abitur/aspirantura/""", callback.from_user.id, callback.message.message_id, reply_markup=markup)
 
     if callback.data == "back":
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
         greet(callback.message)
+
 
 bot.infinity_polling()
